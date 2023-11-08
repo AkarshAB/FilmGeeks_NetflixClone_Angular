069959f0fa95e4d5de93e6aa6778cc8b537f7942
AkarshBharathan <akarshab361@gmail.com>
2023-11-08 12:28:03 +0530

    NetflixClone_Backend

---
+++ services/db.js
@@ -0,0 +1,75 @@
+//1. import mongoose
+
+const mongoose = require('mongoose');
+
+//2. define connection string
+
+mongoose.connect('mongodb://localhost:27017/Films', () => {
+    console.log('connected to MongoDB');
+})
+
+//3.model creation 
+
+const User = mongoose.model('User',
+    {
+        uname: String,
+        fullName: String,
+        pswd: String,
+        watchlist: Array
+    })
+
+const Movie = mongoose.model('Movie', {
+    id: Number,
+    page: Number,
+    results: Object,
+    total_pages: Number,
+    total_results: Number,
+})
+
+//trending model creation
+
+const Trending = mongoose.model('Trending', {
+    id: Number,
+    page: Number,
+    results: Object,
+    total_pages: Number,
+    total_results: Number,
+})
+
+
+//watchlist model creation
+const Watchlist = mongoose.model('Watchlist', {
+    usrname: String,
+    id: Number,
+    title: String,
+    name: String,
+    original_title: String,
+    original_name: String,
+    overview: String,
+    poster_path: String,
+    media_type: String,
+    vote_average: Number,
+    release_date: String,
+    first_air_date: String
+})
+
+//review model creation
+const Review = mongoose.model('Review', {
+    usrname: String,
+    author: String,
+    id: Number,
+    rating: Number,
+    comment: String,
+    reviewTime: String,
+})
+
+
+
+
+module.exports = {
+    User,
+    Movie,
+    Trending,
+    Watchlist,
+    Review,
+}
